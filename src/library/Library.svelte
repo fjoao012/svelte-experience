a44f04f06ceeb512a4db93c25a733fec52f54c0b
João Ferreira (TT-CH/ERS-Av) <joao.ferreira3@pt.bosch.com>
2021-04-09 17:10:01 +0100

    (Pluralsight course)
    Add files for the book library (until chapter 5)

---
+++ src/library/Library.svelte
@@ -0,0 +1,45 @@
+<script>
+    // https://github.com/jaketrent/svelte-getting-started/tree/master/m04-build-your-first-svelte-components/snippets
+    import BookGrid from './BookGrid.svelte';
+    import Button from '../common/Button.svelte';
+    import {httpGet} from '../common/api.js';
+    import { onMount } from 'svelte';
+
+    let books = [];
+    onMount(async function() {
+        const {data} = await httpGet('/?_sort=id&_order=desc');
+        books = data;
+    })
+</script>
+
+<style>
+  header {
+    margin: var(--spacingMedium) 0 var(--spacingLarge) 0;
+    text-transform: uppercase;
+  }
+  .preamble {
+    display: block;
+  }
+  h1 {
+    font-size: var(--typeSizeXXLarge);
+    font-weight: var(--typeWeightBold);
+    line-height: var(--typeLineHeightTight);
+  }
+
+  .greeting {
+    font-size: var(--typeSizeSmall);
+  }
+</style>
+
+<header>
+    <span class="preamble">Welcome to the </span>
+    <h1>Library</h1>
+</header>
+
+<p class="greeting">
+    This is a library for the people. Welcome! Read the books here. Be inspired. 
+    Go home, and share them with your family.
+</p>
+
+<Button>+ Add Book</Button>
+<BookGrid books={books}></BookGrid>
